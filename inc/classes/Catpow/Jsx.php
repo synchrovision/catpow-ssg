93ce39671d7b1369c3460213adffdbe95f1d1843
synchrovision <trinitybluster@yahoo.co.jp>
2021-06-14 17:41:32 +0900

    refactor :recycle: share common process

--- inc/classes/Catpow/Jsx.php
+++ inc/classes/Catpow/Jsx.php
@@ -9,6 +9,12 @@ class Jsx{
 			return self::bundle($entry_jsx_file,$file);
 		}
 	}
+	public static function init(){
+		putenv('PATH='.getenv('PATH').':'.INC_DIR.':'.INC_DIR.'/node_modules/.bin');
+		putenv('NODE_PATH='.getenv('NODE_PATH').':'.INC_DIR.'/node_modules');
+		chdir(INC_DIR);
+		if(!file_exists(INC_DIR.'/node_modules')){passthru('npm install');}
+	}
 	public static function get_jsx_file_for_file($file){
 		$jsx_file=substr($file,0,-2).'jsx';
 		if(file_exists($jsx_file)){return $jsx_file;}
@@ -18,10 +24,7 @@ class Jsx{
 		return false;
 	}
 	public static function compile($jsx_file,$js_file){
-		putenv('PATH='.getenv('PATH').':'.INC_DIR.':'.INC_DIR.'/node_modules/.bin');
-		putenv('NODE_PATH='.getenv('NODE_PATH').':'.INC_DIR.'/node_modules');
-		chdir(INC_DIR);
-		if(!file_exists(INC_DIR.'/node_modules')){passthru('npm install');}
+		self::init();
 		if(!file_exists($jsx_file)){return;}
 		if(!file_exists($js_file) or filemtime($js_file) < filemtime($jsx_file)){
 			passthru('babel '.$jsx_file.' -o '.$js_file);
@@ -36,10 +39,7 @@ class Jsx{
 		return false;
 	}
 	public static function bundle($entry_jsx_file,$bundle_js_file){
-		putenv('PATH='.getenv('PATH').':'.INC_DIR.':'.INC_DIR.'/node_modules/.bin');
-		putenv('NODE_PATH='.getenv('NODE_PATH').':'.INC_DIR.'/node_modules');
-		chdir(INC_DIR);
-		if(!file_exists(INC_DIR.'/node_modules')){passthru('npm install');}
+		self::init();
 		if(!file_exists($entry_jsx_file)){return;}
 		if(!file_exists($bundle_js_file) or filemtime($bundle_js_file) < filemtime($entry_jsx_file)){
 			passthru('npx webpack build --entry '.$entry_jsx_file.' -o '.dirname($bundle_js_file).' --output-filename '.basename($bundle_js_file));
