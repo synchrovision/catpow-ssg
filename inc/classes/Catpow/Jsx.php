05a4c47b6fd77c8b70eb1d3f936b8ad7250bb8de
synchrovision <trinitybluster@yahoo.co.jp>
2021-06-15 14:22:41 +0900

    feat :+1: consider bundle files modified time

--- inc/classes/Catpow/Jsx.php
+++ inc/classes/Catpow/Jsx.php
@@ -33,15 +33,19 @@ class Jsx{
 	public static function get_entry_jsx_file_for_file($file){
 		$entry_jsx_file=substr($file,0,-3).'/index.jsx';
 		if(file_exists($entry_jsx_file)){return $entry_jsx_file;}
-		if(file_exists($f=str_replace('/js/','/_jsx/',$jsx_file))){return $f;}
-		if(file_exists($f=str_replace(ABSPATH,TMPL_DIR,$jsx_file))){return $f;}
-		if(file_exists($f=str_replace([ABSPATH,'/js/'],[TMPL_DIR,'/_jsx/'],$jsx_file))){return $f;}
+		if(file_exists($f=str_replace('/js/','/_jsx/',$entry_jsx_file))){return $f;}
+		if(file_exists($f=str_replace(ABSPATH,TMPL_DIR,$entry_jsx_file))){return $f;}
+		if(file_exists($f=str_replace([ABSPATH,'/js/'],[TMPL_DIR,'/_jsx/'],$entry_jsx_file))){return $f;}
 		return false;
 	}
 	public static function bundle($entry_jsx_file,$bundle_js_file){
 		self::init();
 		if(!file_exists($entry_jsx_file)){return;}
-		if(!file_exists($bundle_js_file) or filemtime($bundle_js_file) < filemtime($entry_jsx_file)){
+		$latest_filetime=filemtime($entry_jsx_file);
+		foreach(glob(dirname($entry_jsx_file).'/*') as $bundle_file){
+			$latest_filetime=max($latest_filetime,filemtime($bundle_file));
+		}
+		if(!file_exists($bundle_js_file) or filemtime($bundle_js_file) < $latest_filetime){
 			passthru('npx webpack build --entry '.$entry_jsx_file.' -o '.dirname($bundle_js_file).' --output-filename '.basename($bundle_js_file));
 		}
 	}
