c0c0d0c689460187e705bfd243095d9674aadb60
synchrovision <trinitybluster@yahoo.co.jp>
2021-06-18 19:55:11 +0900

    fix :bug: fix args

--- inc/classes/Catpow/BEM.php
+++ inc/classes/Catpow/BEM.php
@@ -78,7 +78,7 @@ class BEM{
 	public function export_selectors_file($fname='_scss/selectors.scss'){
 		$file=PAGE_TMPL_DIR.'/'.$fname;
 		$dir=dirname($file);
-		if(!is_dir($dir)){mkdir($dir,true);}
+		if(!is_dir($dir)){mkdir($dir,0755,true);}
 		file_put_contents($file,self::get_selectors_code($this->selectors));
 	}
 	public static function get_selectors_code($selectors,$indent=0){
