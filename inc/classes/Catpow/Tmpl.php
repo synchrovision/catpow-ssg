6a825fe775d76e097c627780c2d10a502ae43c47
synchrovision <trinitybluster@yahoo.co.jp>
2021-06-23 10:28:00 +0900

    feat :+1: enable to get template file by sitemap config

--- inc/classes/Catpow/Tmpl.php
+++ inc/classes/Catpow/Tmpl.php
@@ -2,12 +2,12 @@
 namespace Catpow;
 class Tmpl{
 	public static function compile_for_file($file){
-		if($tmpl_file=self::get_tmpl_file_for_file($file)){
-			global $page;
+		$uri=preg_replace('/index\.(html?|php)$/','',str_replace(ABSPATH,'',$file));
+		if(($tmpl_file=self::get_tmpl_file_for_file($file)) || ($tmpl_file=self::get_tmpl_file_for_uri($uri))){
 			ob_start();
-			$uri=preg_replace('/(index)?\.(html?|php)$/','',str_replace(ABSPATH,'',$file));
 			Page::init($uri);
 			try{
+				global $sitemap,$page;
 				include $tmpl_file;
 				if(!is_dir(dirname($file))){
 					mkdir(dirname($file),0755,true);
@@ -18,11 +18,19 @@ class Tmpl{
 				ob_end_clean();
 				error_log($e->getMessage());
 			}
-		}
+		} 
 	}
 	public static function get_tmpl_file_for_file($file){
+		global $sitemap;
 		if(file_exists($f=$file.'.tmpl.php')){return $f;}
 		if(file_exists($f=str_replace(ABSPATH,TMPL_DIR,$file).'.php')){return $f;}
+		return false;
+	}
+	public static function get_tmpl_file_for_uri($uri){
+		global $sitemap;
+		if(empty($sitemap[$uri]['template'])){return false;}
+		if(file_exists($f=ABSPATH.$sitemap[$uri]['template'])){return $f;}
+		if(file_exists($f=TMPL_DIR.$sitemap[$uri]['template'])){return $f;}
 		return false;
 	}
 }
