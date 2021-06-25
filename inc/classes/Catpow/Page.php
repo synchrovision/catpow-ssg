036d994e0f178fd9066c933784a1717ba8262e0e
synchrovision <trinitybluster@yahoo.co.jp>
2021-06-25 18:44:58 +0900

    feat :+1: add get_the_file method

--- inc/classes/Catpow/Page.php
+++ inc/classes/Catpow/Page.php
@@ -1,10 +1,11 @@
 <?php
 namespace Catpow;
 class Page{
-	public $uri,$info,$scripts,$styles;
+	public $uri,$dir_uri,$info,$scripts,$styles;
 	private static $instance;
 	private function __construct($uri,$info){
 		$this->uri=$uri;
+		$this->dir=(substr($uri,-1)==='/')?$uri:dirname($uri).'/';
 		$this->scripts=new Deps('js');
 		$this->styles=new Deps('css');
 		if(empty($info)){
@@ -14,6 +15,13 @@ class Page{
 	}
 	public static function init($uri,$info=null){
 		return $GLOBALS['page']=static::$instance=new static($uri,$info);
+	}
+	public function get_the_file($file){
+		if(file_exists($f=ABSPATH.$this->dir.$file)){return $f;}
+		if(file_exists($f=TMPL_DIR.$this->dir.$file)){return $f;}
+		if(file_exists($f=ABSPATH.'/'.$file)){return $f;}
+		if(file_exists($f=TMPL_DIR.'/'.$file)){return $f;}
+		if(file_exists($f=INC_DIR.'/'.$file)){return $f;}
 	}
 	public function use_block($block){
 		if(Block::get_block_file($block,'app/index.jsx')){
