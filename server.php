6aaf35fb9549189d9788ec764dc1d62833ec079b
synchrovision <trinitybluster@yahoo.co.jp>
2021-06-30 13:29:57 +0900

    feat :+1: load .env if exists

--- server.php
+++ server.php
@@ -86,6 +86,10 @@ function init(){
 		if(file_exists($f=CONF_DIR.'/classes/'.str_replace('\\','/',$class).'.php')){include($f);return;}
 		if(file_exists($f=INC_DIR.'/classes/'.str_replace('\\','/',$class).'.php')){include($f);}
 	});
+	if(file_exists(APP_DIR.'/.env')){
+		$dotenv = Dotenv\Dotenv::createImmutable(APP_DIR);
+		$dotenv->load();
+	}
 	if(file_exists($f=CONF_DIR.'/functions.php')){include($f);}
 	if(file_exists($f=INC_DIR.'/functions.php')){include($f);}
 }
