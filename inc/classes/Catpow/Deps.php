a72d9aad5e744a4fc842d30ac03bf4d90291c75e
synchrovision <trinitybluster@yahoo.co.jp>
2022-03-14 20:51:20 +0900

    feat :+1: add materialicons as preset

--- inc/classes/Catpow/Deps.php
+++ inc/classes/Catpow/Deps.php
@@ -35,6 +35,7 @@ class Deps{
 			]
 		],
 		'css'=>[
+			'materialicons'=>['src'=>'https://fonts.googleapis.com/icon?family=Material+Icons'],
 			'bootstrap'=>[
 				'src'=>'https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css',
 				'attr'=>'integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous"'
