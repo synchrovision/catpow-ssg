6230cdc086d418fc2bef8247a205e33233acb6d6
synchrovision <trinitybluster@yahoo.co.jp>
2022-04-26 15:52:46 +0900

    feat :+1: add TabPanel component

---
+++ inc/components/cp/TabPanel.jsx
@@ -0,0 +1,16 @@
+﻿export const TabPanel=(props)=>{
+	const {useState,useCallback,useEffect,useRef,useReducer}=React;
+	const {className="tabpanel",children}=props;
+	const [current,setCurrent]=useState(props.initialOpen || 0);
+	
+	return (
+		<div className={className}>
+			<div className="tabs">
+			{children.map((child,index)=>(
+				<div className={"tab"+(current===index?' -active':'')} onClick={()=>setCurrent(index)} key={child.key}>{child.key}</div>
+			))}
+			</div>
+			<div class="contents">{children[current]}</div>
+		</div>
+	);
+}
--- inc/components/cp/index.js
+++ inc/components/cp/index.js
@@ -1,3 +1,4 @@
 export {CP} from './CP.jsx';
 export {Slider} from './Slider.jsx';
+export {TabPanel} from './TabPanel.jsx';
 export {Transition} from './Transition.jsx';
