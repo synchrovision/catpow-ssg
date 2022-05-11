cfe29b61d94cf211265b7cfd7f5fe148e71ed96e
synchrovision <trinitybluster@yahoo.co.jp>
2022-05-11 13:43:12 +0900

    feat :+1: add el method

--- inc/components/cp/functions.jsx
+++ inc/components/cp/functions.jsx
@@ -17,3 +17,18 @@ export const renderComponents=()=>{
 		ReactDOM.render(React.createElement(Catpow[el.dataset.cpComponent]),{...el.dataset});
 	});
 };
+export const el=(tag,props,children)=>{
+	const el=document.createElement(tag);
+	Object.keys(props).forEach((key)=>{
+		el[key]=props[key];
+	});
+	if(Array.isArray(children)){
+		children.forEach((child)=>{
+			if(typeof child === 'string'){
+				el.appendChild(document.createTextNode(child));
+			}
+			else{el.appendChild(child);}
+		});
+	}
+	return el;
+};
