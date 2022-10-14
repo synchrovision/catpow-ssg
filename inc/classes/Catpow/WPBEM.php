26dea420f2792614ec37e37884dad3a97b339288
synchrovision <trinitybluster@yahoo.co.jp>
2022-10-14 16:24:42 +0900

    change :wrench: do not skip template tag

--- inc/classes/Catpow/WPBEM.php
+++ inc/classes/Catpow/WPBEM.php
@@ -35,7 +35,7 @@ class WPBEM extends CssRule{
 	private function _apply($el){
 		if(!is_a($el,\DOMElement::class)){return;}
 		if(empty($el->getAttribute('class'))){
-			if(in_array($el->tagName,['br','link','script','source','template'],true)){return;}
+			if(in_array($el->tagName,['br','link','script','source'],true)){return;}
 			$el->setAttribute('class','_'.$el->tagName);
 		}
 		$classes=explode(' ',$el->getAttribute('class')??'');
