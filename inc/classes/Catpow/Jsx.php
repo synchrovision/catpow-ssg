91a11c24551f813484f25885060f1895126973af
synchrovision <trinitybluster@yahoo.co.jp>
2022-11-07 12:46:35 +0900

    feat :+1: use deno to compile jsx

--- inc/classes/Catpow/Jsx.php
+++ inc/classes/Catpow/Jsx.php
@@ -9,12 +9,6 @@ class Jsx{
 			return self::bundle($entry_file,$file);
 		}
 	}
-	public static function init(){
-		putenv('PATH='.getenv('PATH').':'.INC_DIR.':'.INC_DIR.'/node_modules/.bin');
-		putenv('NODE_PATH='.getenv('NODE_PATH').':'.INC_DIR.'/node_modules');
-		chdir(INC_DIR);
-		if(!file_exists(INC_DIR.'/node_modules')){passthru('npm install');}
-	}
 	public static function get_jsx_file_for_file($file){
 		$jsx_file=substr($file,0,-2).'jsx';
 		if(file_exists($jsx_file)){return $jsx_file;}
@@ -24,11 +18,11 @@ class Jsx{
 		return false;
 	}
 	public static function compile($jsx_file,$js_file){
-		self::init();
+		chdir(INC_DIR);
 		if(!file_exists($jsx_file)){return;}
 		if(!file_exists($js_file) or filemtime($js_file) < filemtime($jsx_file)){
 			ob_start();
-			passthru('babel '.$jsx_file.' -o '.$js_file);
+			passthru("deno bundle {$jsx_file} {$js_file}");
 			error_log(ob_get_clean());
 		}
 	}
@@ -49,7 +43,7 @@ class Jsx{
 		return false;
 	}
 	public static function bundle($entry_file,$bundle_js_file){
-		self::init();
+		chdir(INC_DIR);
 		if(!file_exists($entry_file)){return;}
 		$latest_filetime=filemtime($entry_file);
 		foreach(glob(dirname($entry_file).'/*') as $bundle_file){
@@ -57,7 +51,7 @@ class Jsx{
 		}
 		if(!file_exists($bundle_js_file) or filemtime($bundle_js_file) < $latest_filetime){
 			ob_start();
-			passthru('npx webpack build --mode production --entry '.$entry_file.' -o '.dirname($bundle_js_file).' --output-filename '.basename($bundle_js_file));
+			passthru("deno bundle {$entry_file} {$bundle_js_file}");
 			error_log(ob_get_clean());
 		}
 	}
