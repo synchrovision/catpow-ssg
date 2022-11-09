fffb88d9c4969d3d3a9a9abbb491c9d6593fc093
synchrovision <trinitybluster@yahoo.co.jp>
2022-11-09 18:16:06 +0900

    feat :+1: add TextBuild component

---
+++ inc/modules/component/TextBuild.jsx
@@ -0,0 +1,41 @@
+﻿
+export const TextBuild=(props)=>{
+	const {useState,useCallback,useMemo,useEffect}=React;
+	const {className="cp-textbuild",children,delay=5,interval=150}=props;
+	
+	const [progress,setProgress]=useState(0);
+	
+	const letters=useMemo(()=>{
+		const letters=[];
+		children.forEach((line)=>{
+			if(typeof line === 'string'){
+				Array.from(line).forEach((letter)=>letters.push(letter));
+			}
+			else{
+				letters.push(line);
+			}
+		});
+		return letters;
+	},[children]);
+	
+	useEffect(()=>{
+		let progress=-delay; 
+		const timer=setInterval(()=>{
+			progress++;
+			setProgress(progress);
+			if(progress>letters.length){
+				clearInterval(timer);
+			}
+		},interval);
+		return ()=>clearInterval(timer);
+	},[letters,delay,interval]);
+	
+	return (
+		<div className={className}>
+			{letters.map((letter,index)=>(
+				(typeof letter === 'string')?
+				(<span className={className+'__letter is-' + (index<progress?'show':'hide')}>{letter}</span>):letter
+			))}
+		</div>
+	);
+}
--- inc/modules/component/index.js
+++ inc/modules/component/index.js
@@ -8,4 +8,5 @@ export * from './RawNode.jsx';
 export * from './ScrollSpy.jsx';
 export * from './Slider.jsx';
 export * from './TabPanel.jsx';
+export * from './TextBuild.jsx';
 export * from './Transition.jsx';
