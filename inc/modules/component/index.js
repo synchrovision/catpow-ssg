a042890710be0d368021502d3c7e1f2426a1a5e5
synchrovision <trinitybluster@yahoo.co.jp>
2022-11-09 17:09:28 +0900

    feat :+1: add RawNode component

--- inc/modules/component/index.js
+++ inc/modules/component/index.js
@@ -4,6 +4,7 @@ export * from './Animation/index.js';
 export * from './SVG/index.js';
 
 export * from './Parallax.jsx';
+export * from './RawNode.jsx';
 export * from './ScrollSpy.jsx';
 export * from './Slider.jsx';
 export * from './TabPanel.jsx';
