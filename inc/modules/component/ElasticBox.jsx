bf65219e55b277d9911b6641214c9e3c86e7984c
synchrovision <trinitybluster@yahoo.co.jp>
2022-12-01 14:30:05 +0900

    feat :+1: add ElasticBox component

---
+++ inc/modules/component/ElasticBox.jsx
@@ -0,0 +1,27 @@
+﻿import {bem} from 'util';
+
+export const ElasticBox=(props)=>{
+	const {useMemo,useState,useCallback,useEffect,useRef}=React;
+	const {className="cp-elasticbox",children}=props;
+	const [current,setCurrent]=useState(props.initialOpen || 0);
+	const classes=useMemo(()=>bem(className),[className]);
+	
+	const ref=useRef({});
+	
+	const [height,setHeight]=useState('auto');
+	
+	useEffect(()=>{
+		const observer=new ResizeObserver((entries)=>{
+			setHeight(entries[0].contentRect.height + 'px');
+		});
+		observer.observe(ref.current.children[0]);
+	},[ref.current]);
+	
+	return (
+		<div className={classes()} ref={ref} style={{height}}>
+			<div className={classes._body()}>
+				{props.children}
+			</div>
+		</div>
+	);
+}
--- inc/modules/component/index.js
+++ inc/modules/component/index.js
@@ -3,6 +3,7 @@ export * from './functions.jsx';
 export * from './Animation/index.js';
 export * from './SVG/index.js';
 
+export * from './ElasticBox.jsx';
 export * from './Parallax.jsx';
 export * from './RawNode.jsx';
 export * from './ScrollSpy.jsx';
