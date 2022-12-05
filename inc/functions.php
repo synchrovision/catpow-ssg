7ca9ac1d9eb36efe5dea771a81a3ef0ba3df055d
synchrovision <trinitybluster@yahoo.co.jp>
2022-12-05 13:34:07 +0900

    feat :+1: enable to apply button style at rtf function

--- inc/functions.php
+++ inc/functions.php
@@ -182,6 +182,7 @@ function rtf($text,$pref='rtf'){
 	$text=preg_replace('/■ (.+)/u','<h4 class="'.$pref.'-title">$1</h4>',$text);
 	$text=preg_replace('/!\[(.+?)\]\((.+?)\)/u','<img class="'.$pref.'-image" src="$2" alt="$1"/>',$text);
 	$text=preg_replace('/\[tel:((\d+)\-(\d+)\-(\d+))\]/u','<a class="'.$pref.'-tel" href="tel:$2$3$4" target="_brank">$1</a>',$text);
+	$text=preg_replace('/\[\[(.+?)\]\]\((.+?)\)/u','<a class="'.$pref.'-button" href="$2" target="_brank"><span class="'.$pref.'-button__label">$1</span></a>',$text);
 	$text=preg_replace('/\[(.+?)\]\((.+?)\)/u','<a class="'.$pref.'-link" href="$2" target="_brank">$1</a>',$text);
 	$text=preg_replace('/(.{1,8}?)：(.+)/u','<dl class="'.$pref.'-dl"><dt class="'.$pref.'-dl__dt">$1</dt><dd class="'.$pref.'-dl__dd">$2</dd></dl>',$text);
 	$text=preg_replace('/^・ (.+(\n　.+)*)$/um','<ul class="'.$pref.'-ul"><li class="'.$pref.'-ul__li">$1</li></ul>',$text);
