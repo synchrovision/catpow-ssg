59af267b7c697dddfac0af93e6377ca913ec3b88
synchrovision <trinitybluster@yahoo.co.jp>
2022-12-12 19:53:37 +0900

    feat :+1: use translate3d for dimensionBox

--- inc/modules/util/dimension.jsx
+++ inc/modules/util/dimension.jsx
@@ -22,12 +22,13 @@ export const dimensionBox=(box)=>{
 	const tick=(t)=>{
 		const bnd=box.getBoundingClientRect(),wh=window.innerHeight;
 		body.style.setProperty('perspective-origin','center '+(wh/2-bnd.top)+'px');
-		body.style.setProperty('top',bnd.top+'px');
+		body.style.setProperty('transform','translate3d(0,'+bnd.top+'px,0)');
 		window.requestAnimationFrame(tick);
 	}
 	window.requestAnimationFrame(tick);
 	body.style.setProperty('position','fixed');
 	body.style.setProperty('overflow','hidden');
+	body.style.setProperty('top',0);
 	body.style.setProperty('left',0);
 	body.style.setProperty('right',0);
 	box.style.setProperty('height',body.getBoundingClientRect().height + 'px');
