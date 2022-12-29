836f5c5fbc559835d40f4c7b122895c26111dc40
synchrovision <trinitybluster@yahoo.co.jp>
2022-12-29 23:25:03 +0900

    feat :+1: add BG class

---
+++ inc/classes/Catpow/SVG/BG.php
@@ -0,0 +1,9 @@
+<?php
+namespace Catpow\SVG;
+class BG extends Shape{
+	protected $default_atts=['x'=>0,'y'=>0,'width'=>'100%','height'=>'100%'];
+	public function render(){
+		$className=empty($this->props['className'])?'':' '.$this->props['className'];
+		printf('<rect class="%s%s"%s/>',$this->className,$className,$this->get_attributes());
+	}
+}
