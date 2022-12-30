7bfc1839ef1ad4cdbb8895fdcc2ffa9f7721a8c3
synchrovision <trinitybluster@yahoo.co.jp>
2022-12-30 13:02:42 +0900

    feat :+1: update Colors class

--- inc/classes/Catpow/Colors.php
+++ inc/classes/Catpow/Colors.php
@@ -3,6 +3,23 @@ namespace Catpow;
 
 class Colors{
 	const NAMED_COLORS=[
+		"black"=>"#000000",
+		"silver"=>"#c0c0c0",
+		"gray"=>"#808080",
+		"white"=>"#ffffff",
+		"maroon"=>"#800000",
+		"red"=>"#ff0000",
+		"purple"=>"#800080",
+		"fuchsia"=>"#ff00ff",
+		"green"=>"#008000",
+		"lime"=>"#00ff00",
+		"olive"=>"#808000",
+		"yellow"=>"#ffff00",
+		"navy"=>"#000080",
+		"blue"=>"#0000ff",
+		"teal"=>"#008080",
+		"aqua"=>"#00ffff",
+		"orange"=>"#ffa500",
 		"aliceblue"=>"#f0f8ff",
 		"antiquewhite"=>"#faebd7",
 		"aquamarine"=>"#7fffd4",
