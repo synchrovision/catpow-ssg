f71e6d29f1881cb54b83ae245bf54a0758ad975c
synchrovision <trinitybluster@yahoo.co.jp>
2023-01-01 13:39:20 +0900

    feat :+1: update Shape class

--- inc/classes/Catpow/SVG/Shape.php
+++ inc/classes/Catpow/SVG/Shape.php
@@ -1,8 +1,8 @@
 <?php
 namespace Catpow\SVG;
-abstract class Shape{
+use Catpow\Scss;
+class Shape extends SVG{
 	public $className,$container,$props;
-	protected $default_atts=[],$optional_atts=["clip-path","clip-rule","color","color-interpolation","color-rendering","cursor","display","fill","fill-opacity","fill-rule","filter","mask","opacity","pointer-events","shape-rendering","stroke","stroke-dasharray","stroke-dashoffset","stroke-linecap","stroke-linejoin","stroke-miterlimit","stroke-opacity","stroke-width","transform","vector-effect","visibility","style","id"];
 	public function __construct($container,$props){
 		$this->className=sprintf('svg-shape svg-shape-%s',strtolower(substr(strrchr(static::class,'\\'),1)));
 		if(!empty($props['className'])){$this->className.=' '.$props['className'];}
@@ -11,15 +11,5 @@ abstract class Shape{
 	}
 	public function render(){
 		
-	}
-	public function get_attributes(){
-		$atts=[];
-		$rtn='';
-		$atts=array_merge($atts,$this->default_atts,array_intersect_key($this->props,$this->default_atts));
-		$atts=array_merge($atts,array_intersect_key($this->props,array_flip($this->optional_atts)));
-		foreach($atts as $key=>$val){
-			$rtn.=sprintf(' %s="%s"',$key,$val);
-		}
-		return $rtn;
 	}
 }
