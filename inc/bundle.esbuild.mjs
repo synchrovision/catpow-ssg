c6371973e539257e25c5b31445806449b1f01897
synchrovision <16245284+synchrovision@users.noreply.github.com>
2023-02-15 13:48:46 +0900

    feat :+1: import svg with own plugin that not Require react as module

--- inc/bundle.esbuild.mjs
+++ inc/bundle.esbuild.mjs
@@ -1,6 +1,9 @@
 import * as esbuild from 'esbuild';
-import {svgrPlugin} from 'esbuild-svgr-plugin';
+import fs from 'fs';
+import path from 'path';
+import {transform } from '@svgr/core';
 import {sassPlugin} from 'esbuild-sass-plugin';
+
 
 let pathResolver={
 	name:'pathResolver',
@@ -27,10 +30,34 @@ let pathResolver={
 		});
 	},
 }
+let svgAsJsx={
+	name:'svgAsJsx',
+	setup(build){
+		build.onResolve({filter:/\.svg$/},(args)=>{
+			return {
+				path:path.join(args.resolveDir,args.path),
+				namespace:/\.[jt]sx?$/.test(args.importer)?'svgr':undefined,
+			}
+		})
+
+		build.onLoad({filter:/.*/,namespace:'svgr'},async({path:pathname})=>{
+			const [filename]=pathname.split('?',2)
+			const dirname=path.dirname(filename)
+			const svg=await fs.promises.readFile(pathname,'utf8')
+			const contents=await transform(svg,{jsxRuntime:'automatic'},{filePath:pathname})
+
+			return {
+				contents,
+				loader: 'jsx',
+				resolveDir:dirname,
+			}
+		})
+	},
+}
 
 await esbuild.build({
 	entryPoints: [process.argv[2]],
 	outfile: process.argv[3],
 	bundle:true,
-	plugins:[pathResolver,svgrPlugin(),sassPlugin()]
+	plugins:[pathResolver,svgAsJsx,sassPlugin()]
 })
