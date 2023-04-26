4834cb84f06a3c24668ac89d8ff6d26a86813ff9
synchrovision <16245284+synchrovision@users.noreply.github.com>
2023-04-26 13:08:51 +0900

    feat :+1: enable to observe width

--- inc/modules/component/ElasticBox.jsx
+++ inc/modules/component/ElasticBox.jsx
@@ -2,23 +2,26 @@
 
 export const ElasticBox=(props)=>{
 	const {useMemo,useState,useCallback,useEffect,useRef}=React;
-	const {className="cp-elasticbox",children}=props;
+	const {className="cp-elasticbox",children,observeHeight=true,observeWidth=false}=props;
 	const [current,setCurrent]=useState(props.initialOpen || 0);
 	const classes=useMemo(()=>bem(className),[className]);
 	
 	const ref=useRef({});
 	
-	const [height,setHeight]=useState('auto');
+	const [height,setHeight]=useState(observeHeight?'auto':'');
+	const [width,setWidth]=useState(observeWidth?'auto':'');
 	
 	useEffect(()=>{
 		const observer=new ResizeObserver((entries)=>{
-			setHeight(entries[0].contentRect.height + 'px');
+			if(observeHeight){setHeight(entries[0].contentRect.height + 'px');}
+			if(observeWidth){setWidth(entries[0].contentRect.width + 'px');}
 		});
 		observer.observe(ref.current.children[0]);
-	},[ref.current]);
+		return ()=>observer.disconnect();
+	},[ref.current,observeHeight,observeWidth]);
 	
 	return (
-		<div className={classes()} ref={ref} style={{height}}>
+		<div className={classes()} ref={ref} style={{height,width}}>
 			<div className={classes._body()}>
 				{props.children}
 			</div>
