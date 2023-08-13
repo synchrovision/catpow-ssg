bd65d50dcb3c65283561b57553ae180bdbe2d55d
synchrovision <trinitybluster@yahoo.co.jp>
2023-08-13 23:35:57 +0900

    feat :+1: enable to add custom css var

--- inc/modules/util/dimension.jsx
+++ inc/modules/util/dimension.jsx
@@ -1,14 +1,47 @@
-export const parallax=(el)=>{
+export const parallax=(el,vars={})=>{
 	var wh,ch;
 	const updateCoef=()=>{
 		wh=window.innerHeight,ch=el.clientHeight;
 	};
 	window.addEventListener('resize',updateCoef);
 	updateCoef();
+	const keys=Object.keys(vars);
+	keys.forEach((key)=>{
+		if(Array.isArray(vars[key])){
+			const vs=vars[key];
+			if(vs.length & 1){vs.push(1);}
+			if(vs.length===2){
+				const s=vs[0],c=1/(vs[1]-vs[0]);
+				vars[key]=(p)=>Math.max(0,Math.min((p-s)*c,1));
+			}
+			else{
+				const ss=[],es=[],cs=[];
+				for(let i=0;i<vs.length;i+=2){
+					ss.push(vs[i]);
+					es.push(vs[i+1]);
+					cs.push(1/(vs[i+1]-vs[i]));
+				}
+				vars[key]=(p)=>{
+					for(let i=0;i<ss.length;i++){
+						if(p<es[i]){
+							const a=(p-ss[i])*cs[i];
+							return Math.max(0,Math.min((i&1)?(1-a):a,1));
+						}
+					}
+					return (ss.length&1)?1:0;
+				}
+			}
+		}
+	});
+	
 	const tick=(t)=>{
-		el.style.setProperty('--parallax-t',el.getBoundingClientRect().top);
-		el.style.setProperty('--parallax-c',el.getBoundingClientRect().top+ch/2-wh/2);
-		el.style.setProperty('--parallax-b',wh-el.getBoundingClientRect().bottom);
+		const bnd=el.getBoundingClientRect();
+		const p=Math.min(1,Math.max(0,(wh-bnd.top)/(bnd.height+wh)));
+		el.style.setProperty('--parallax-t',bnd.top);
+		el.style.setProperty('--parallax-c',bnd.top+ch/2-wh/2);
+		el.style.setProperty('--parallax-b',wh-bnd.bottom);
+		el.style.setProperty('--parallax-p',p);
+		keys.map((key)=>el.style.setProperty('--'+key,vars[key](p)));
 		window.requestAnimationFrame(tick);
 	}
 	window.requestAnimationFrame(tick);
