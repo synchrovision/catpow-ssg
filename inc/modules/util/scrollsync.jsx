a30ada059aba4fd5cd604b8dda9264cc92fff23f
synchrovision <trinitybluster@yahoo.co.jp>
2023-08-13 18:53:05 +0900

    feat :+1: scroll by nav click

--- inc/modules/util/scrollsync.jsx
+++ inc/modules/util/scrollsync.jsx
@@ -1,5 +1,6 @@
 export const scrollsync=function(el,param={}){
 	const app={};
+	app.param=Object.assign({nav:false,direction:'y',margin:0},param);
 	const items=el.children;
 	const updateActiveItem=()=>{
 		let i,index=-1;
@@ -13,10 +14,35 @@ export const scrollsync=function(el,param={}){
 				break;
 			}
 		}
+		updateItemsClass(el.children,index);
+		if(app.param.nav){
+			if(Array.isArray(app.param.nav)){
+				app.param.nav.forEach((target)=>{updateItemsClass(target.children,index);});
+			}
+			else{
+				updateItemsClass(app.param.nav.children,index);
+			}
+		}
 		app.current=index;
 		el.style.setProperty('--scroll-index',index);
 	};
+	const updateItemsClass=(items,index)=>{
+		const l=items.length;
+		for(let i=0;i<l;i++){
+			const item=items[i];
+			const p=i-index;
+			item.classList.toggle('is-before',p<0);
+			item.classList.toggle('is-prev',p===-1);
+			item.classList.toggle('is-current',p===0);
+			item.classList.toggle('is-next',p===1);
+			item.classList.toggle('is-after',p>0);
+			item.style.setProperty('--scroll-p',p);
+		}
+	};
 	app.goto=(index)=>{
+		const bnd1=el.getBoundingClientRect();
+		const bnd2=el.children[index].getBoundingClientRect();
+		el.scrollBy({[app.param.direction==='x'?'left':'top']:bnd2.top-bnd1.top+app.param.margin});
 		updateActiveItem();
 	};
 	app.prev=()=>app.goto(app.current-1);
@@ -27,12 +53,12 @@ export const scrollsync=function(el,param={}){
 			items[i].addEventListener('click',()=>app.goto(i));
 		}
 	}
-	if(param.nav){
-		if(Array.isArray(param.nav)){
-			param.nav.forEach((target)=>{registerAsNav(target.children);});
+	if(app.param.nav){
+		if(Array.isArray(app.param.nav)){
+			app.param.nav.forEach((target)=>{registerAsNav(target.children);});
 		}
 		else{
-			registerAsNav(param.nav.children);
+			registerAsNav(app.param.nav.children);
 		}
 	}
 	app.timer=setInterval(updateActiveItem,100);
