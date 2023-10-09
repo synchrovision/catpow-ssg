d5b1da1d7234a3553b6858596d3ba15278479437
synchrovision <hatano@catpow.info>
2023-10-09 12:16:59 +0900

    feat :+1: update scrollsync function

--- inc/modules/util/scrollsync.jsx
+++ inc/modules/util/scrollsync.jsx
@@ -2,12 +2,12 @@ import {debounce} from './debounce.jsx';
 
 export const scrollsync=function(el,param={}){
 	const app={};
-	app.param=Object.assign({nav:false},param);
+	app.param=Object.assign({nav:false,items:el.children},param);
 	if(!app.param.direction){
 		app.param.direction=(el.clientHeight<el.scrollHeight)?'y':'x';
 	}
 	if(!app.param.position){
-		const ssa=getComputedStyle(el.children[0])['scroll-snap-align'];
+		const ssa=getComputedStyle(app.param.items[0])['scroll-snap-align'];
 		app.param.position={start:0,center:0.5,end:1}[ssa] || 0;
 	}
 	const k1={x:'left',y:'top'}[app.param.direction];
@@ -17,24 +17,22 @@ export const scrollsync=function(el,param={}){
 	const k5={x:'scrollWidth',y:'scrollHeight'}[app.param.direction];
 	const ps={x:'paddingLeft',y:'paddingTop'}[app.param.direction];
 	const pe={x:'paddingRight',y:'paddingBottom'}[app.param.direction];
-	const items=el.children;
+	if(!app.param.padding){
+		const ssa=getComputedStyle(app.param.items[0])['scroll-snap-align'];
+		app.param.padding=parseFloat(getComputedStyle(el)['scroll-padding-'+(ssa==='end'?k2:k1)]);
+	}
 	const updateActiveItem=()=>{
 		let i,index=-1;
-		const l=el.children.length;
+		const l=app.param.items.length;
 		const bnd=el.getBoundingClientRect();
-		const c=bnd[k1]+bnd[k4]*app.param.position;
+		const c=bnd[k1]+app.param.padding+bnd[k4]*app.param.position;
 		for(i=0;i<l;i++){
-			const bnd=el.children[i].getBoundingClientRect();
+			const bnd=app.param.items[i].getBoundingClientRect();
 			if(bnd[k2]>c){index=i;break;}
 		}
-		updateItemsClass(el.children,index);
-		if(app.param.nav){
-			if(Array.isArray(app.param.nav)){
-				app.param.nav.forEach((target)=>{updateItemsClass(target.children,index);});
-			}
-			else{
-				updateItemsClass(app.param.nav.children,index);
-			}
+		updateItemsClass(app.param.items,index);
+		if(app.param.navItemsList){
+			app.param.navItemsList.forEach((items)=>updateItemsClass(items,index));
 		}
 		if(app.param.controls){
 			if(app.param.controls.prev){app.param.controls.prev.classList.toggle('is-disabled',index<=0);}
@@ -62,19 +60,21 @@ export const scrollsync=function(el,param={}){
 		const p=app.param.position;
 		const ls=bnd[k1]+bnd[k4]*p-el[k3];
 		const le=ls+el[k5]-bnd[k4];
-		for(let i=0;i<el.children.length;i++){
-			const bnd=el.children[i].getBoundingClientRect();
-			app.param.nav.children[i].classList.toggle('is-disabled',bnd[k2]<ls || bnd[k1]>le);
+		for(let i=0;i<app.param.items.length;i++){
+			const bnd=app.param.items[i].getBoundingClientRect();
+			app.param.navItemsList.forEach((items)=>{
+				items[i].classList.toggle('is-disabled',bnd[k2]<ls || bnd[k1]>le);
+			});
 		}
 	}
 	const observer=new ResizeObserver(updateNavClass);
 	observer.observe(el);
 	app.goto=(index)=>{
 		const bnd1=el.getBoundingClientRect();
-		const bnd2=el.children[index].getBoundingClientRect();
+		const bnd2=app.param.items[index].getBoundingClientRect();
 		const p=app.param.position;
 		el.scrollTo({
-			[k1]:el[k3]+(bnd2[k1]+bnd2[k4]*p)-(bnd1[k1]+bnd1[k4]*p)
+			[k1]:el[k3]-app.param.padding+(bnd2[k1]+bnd2[k4]*p)-(bnd1[k1]+bnd1[k4]*p)
 		});
 		updateActiveItem();
 	};
@@ -86,13 +86,20 @@ export const scrollsync=function(el,param={}){
 			items[i].addEventListener('click',()=>app.goto(i));
 		}
 	}
-	if(app.param.nav){
-		if(Array.isArray(app.param.nav)){
-			app.param.nav.forEach((target)=>{registerAsNav(target.children);});
+	const getNavtItemsList=(val)=>{
+		if(Array.isArray(val)){
+			return Array.concat.apply([],val.map(getNavtItemsList));
+		}
+		else if(val instanceof HTMLCollection || val instanceof NodeList){
+			return [val];
 		}
 		else{
-			registerAsNav(app.param.nav.children);
+			return [val.children];
 		}
+	}
+	if(app.param.nav){
+		app.param.navItemsList=getNavtItemsList(app.param.nav);
+		app.param.navItemsList.forEach(registerAsNav);
 	}
 	if(app.param.controls){
 		if(app.param.controls.prev){app.param.controls.prev.addEventListener('click',app.prev);}
