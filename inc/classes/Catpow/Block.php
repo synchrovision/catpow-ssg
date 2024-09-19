8eba127f8a756fc7031a43862fdda32092b1dd11
synchrovision <hatano@catpow.info>
2024-09-20 08:13:08 +0900

    feat :+1: add convert method

--- inc/classes/Catpow/Block.php
+++ inc/classes/Catpow/Block.php
@@ -1,6 +1,7 @@
 <?php
 namespace Catpow;
 class Block{
+	private static $slots=[];
 	public $block,$part='',$props,$children,$dir;
 	public function __construct($block,$props=[],$children=[]){
 		list($this->block,$this->part)=explode('/',$block.'/');
@@ -60,5 +61,65 @@ class Block{
 		if(file_exists($f=TMPL_DIR.'/blocks/'.$block.'/'.$file)){return $f;}
 		if(file_exists($f=INC_DIR.'/blocks/'.$block.'/'.$file)){return $f;}
 		return false;
+	}
+	public static function convert($html){
+		$doc=new \DOMDocument();
+		$doc->loadHTML(mb_encode_numericentity($html,[0x80,0xffff,0,0xffff],'UTF-8'),\LIBXML_HTML_NOIMPLIED|\LIBXML_HTML_NODEFDTD|\LIBXML_NOERROR);
+		foreach($doc->childNodes??[] as $el){
+			self::_convert($el,$doc);
+		}
+		$html=mb_decode_numericentity($doc->saveHTML(),[0x80,0xffff,0,0xffff],'UTF-8');
+		return $html;
+	}
+	private static function _convert($el,$doc){
+		if(!is_a($el,\DOMElement::class) && !is_a($el,\DOMDocumentFragment::class)){return;}
+		if(substr($el->tagName,0,6)==='block-'){
+			$atts=['level'=>self::get_level($el)];
+			foreach($el->attributes as $attr){
+				$atts[$attr->name]=$attr->value;
+			}
+			$id=uniqid();
+			$slot=$doc->createDocumentFragment();
+			if($el->hasChildNodes()){
+				while($el->childNodes->length){
+					$slot->appendChild($el->childNodes->item(0));
+				}
+			}
+			self::$slots[$id]=$slot;
+			$block=new self(substr($el->tagName,6),$atts,sprintf('<slot id="%s"/>',$id));
+			$block->init();
+			$tmp=new \DOMDocument();
+			$tmp->loadHTML(
+				mb_encode_numericentity($block->get_html(),[0x80,0xffff,0,0xffff],'UTF-8'),
+				\LIBXML_HTML_NOIMPLIED|\LIBXML_HTML_NODEFDTD|\LIBXML_NOERROR
+			);
+			$block_el=$doc->importNode($tmp->firstElementChild,true);
+			$el->replaceWith($block_el);
+			$el=$block_el;
+		}
+		elseif($el->tagName==='slot'){
+			$id=$el->getAttribute('id');
+			$slot=self::$slots[$id];
+			$el->replaceWith($slot);
+			$el=$slot;
+			unset(self::$slots[$id]);
+		}
+		if($el->hasChildNodes()){
+			for($i=0;$i<$el->childNodes->length;$i++){
+				self::_convert($el->childNodes->item($i),$doc);
+			}
+		}
+	}
+	public static function get_level($el){
+		if(is_a($el,\DOMElement::class) && $el->hasAttribute('level')){
+			return $el->getAttribute('level');
+		}
+		if(is_a($el->parentNode,\DOMElement::class)){
+			if($el->parentNode->hasAttribute('level')){
+				return $el->parentNode->getAttribute('level')+1;
+			}
+			return self::get_level($el->parentNode);
+		}
+		return 0;
 	}
 }
