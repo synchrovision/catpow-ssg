6188c71a335740248f43cfa3e09807044f64a95b
synchrovision <trinitybluster@yahoo.co.jp>
2024-10-03 01:41:03 +0900

    change :wrench: unify throttle, debounce to buffer

--- inc/modules/util/buffer.jsx
+++ inc/modules/util/buffer.jsx
@@ -1,3 +1,10 @@
+export const debounce=(callback,interval)=>{
+	let timer;
+	return (e)=>{
+		if(timer){clearTimeout(timer);}
+		timer=setTimeout(callback,interval,e);
+	};
+};
 export const throttle=(callback,interval)=>{
 	let timer,hold=false;
 	return (e)=>{
