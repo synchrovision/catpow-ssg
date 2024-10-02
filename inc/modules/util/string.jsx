05514aae1ddc33ef1879dbe7af76f5063a53be3f
synchrovision <trinitybluster@yahoo.co.jp>
2024-10-03 01:41:37 +0900

    refactor :recycle: update wordsToFlags and flagsToWords

--- inc/modules/util/string.jsx
+++ inc/modules/util/string.jsx
@@ -1,13 +1,5 @@
-export const wordsToFlags=(words)=>{
-	var rtn={};
-	if(undefined === words){return {};}
-	words.split(' ').map((word)=>{rtn[word]=true;});
-	return rtn;
-};
-export const flagsToWords=(flags)=>{
-	if(undefined === flags){return '';}
-	return Object.keys(flags).filter((word)=>flags[word]).join(' ');
-};
+export const wordsToFlags=(words)=>words && words.split(' ').reduce((p,c)=>{p[c]=true;return p},{});
+export const flagsToWords=(flags)=>flags && Object.keys(flags).filter((word)=>flags[word]).join(' ');
 export const classNamesToFlags=(classNames)=>classNames && classNames.split(' ').map(kebabToCamel).reduce((p,c)=>{p[c]=true;return p},{});
 export const flagsToClassNames=(flags)=>flags && Object.keys(flags).filter((f)=>flags[f]).map(camelToKebab).join(' ');
 
