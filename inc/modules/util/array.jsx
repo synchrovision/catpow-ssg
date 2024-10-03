4cba4640c921931410fd702f7fb198319fd4543b
synchrovision <trinitybluster@yahoo.co.jp>
2024-10-03 23:31:47 +0900

    change :wrench: unify bsearch and range to array

--- inc/modules/util/array.jsx
+++ inc/modules/util/array.jsx
@@ -10,3 +10,7 @@ export const bsearch=(arr,val)=>{
 	}
 	return m;
 };
+export const range=function*(start,end,step=1){
+	if(arguments.length===1){end=start;start=0;}
+	for(let i=start;i<=end;i+=step){yield i;}
+};
