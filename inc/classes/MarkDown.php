10fee419fbc0765dda6afa1cf78f34150c557243
synchrovision <trinitybluster@yahoo.co.jp>
2024-10-07 22:26:13 +0900

    feat :+1: add header_id_func

--- inc/classes/MarkDown.php
+++ inc/classes/MarkDown.php
@@ -1,11 +1,18 @@
 <?php
 namespace Catpow;
+use Michelf\MarkdownExtra;
 class MarkDown{
 	public static function do_markdown($str){
-		return \Michelf\MarkdownExtra::defaultTransform($str);
+		$parser=new MarkdownExtra;
+		$parser->header_id_func=function($text){
+			return preg_replace('/[^a-z0-9]/','-',strtolower($text));
+		};
+		ob_start();
+		$str=$parser->transform($str);
+		ob_end_clean();
+		return $str;
 	}
 	public static function render($file){
-		echo \Michelf\MarkdownExtra::defaultTransform(file_get_contents($file));
+		echo self::do_markdown(file_get_contents($file));
 	}
 }
-?>
