41bcf8678f997fed4298910b0c46b911225ee63d
synchrovision <trinitybluster@yahoo.co.jp>
2024-10-07 22:30:33 +0900

    feat :+1: update Modules class

--- inc/classes/API/Modules.php
+++ inc/classes/API/Modules.php
@@ -4,7 +4,13 @@ use Catpow\JSX;
 class Modules{
 	public static function request($req){
 		JSX::init();
-		exec('node modules/script/declaration.mjs -t module -v 2020 '.implode(' ',glob(\INC_DIR.'/modules/*/index.js')),$output);
+		$files=[];
+		foreach(scandir(INC_DIR.'/modules/src/') as $fname){
+			if(substr($fname,0,1)!=='.' && file_exists(INC_DIR.'/modules/src/'.$fname.'/index.js')){
+				$files[]='modules/src/'.$fname.'/index.js';
+			}
+		}
+		exec('node modules/src/script/declaration.mjs -t module -v 2020 '.implode(' ',$files),$output);
 		return json_decode(implode("\n",$output));
 	}
 }
