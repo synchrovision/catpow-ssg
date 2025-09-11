f00c4a96c025bca5109c6811a2e9c14873a41786
synchrovision <hatano@catpow.info>
2025-09-11 22:24:19 +0900

    feat 👍 resolve pattern uri

--- inc/classes/API/Index.php
+++ inc/classes/API/Index.php
@@ -5,7 +5,32 @@ class Index{
 	public static function request($req){
 		$site=Site::get_instance();
 		if(!empty($site->sitemap)){
-			return array_keys($site->sitemap);
+			$uris=array_keys($site->sitemap);
+			$results=[];
+			foreach($uris as $uri){
+				if(strpos($uri,'*')!==false){
+					$chunks=explode('/*',$uri);
+					$tmp=[[$chunks[0]]];
+					foreach($chunks as $i=>$chunk){
+						$tmp[$i+1]=[];
+						foreach($tmp[$i] as $chunk_uri){
+							if($index_file=Site::get_config_file_for_uri($chunk_uri,'index')){
+								$index=(function($uri)use($index_file){return include $index_file;})($chunk_uri);
+								foreach($index as $fname=>$info){
+									$tmp[$i+1][]=$chunk_uri.'/'.$fname;
+								}
+							}
+						}
+					}
+					foreach(end($tmp) as $result_uri){
+						$results[$result_uri]=true;
+					}
+				}
+				else{
+					$results[$uri]=true;
+				}
+			}
+			return array_keys($results);
 		}
 		return self::get_html_files_in_dir();
 	}
