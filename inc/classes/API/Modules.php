e31b99e9fa2bb1b01b3967f37f2789c11775c09a
synchrovision <hatano@catpow.info>
2025-12-07 16:40:34 +0900

    feat 👍 update API/Modules class

--- inc/classes/API/Modules.php
+++ inc/classes/API/Modules.php
@@ -5,12 +5,19 @@ class Modules{
 	public static function request($req){
 		JSX::init();
 		$files=[];
-		foreach(scandir(INC_DIR.'/modules/src/') as $fname){
-			if(substr($fname,0,1)!=='.' && file_exists(INC_DIR.'/modules/src/'.$fname.'/index.js')){
-				$files[]='modules/src/'.$fname.'/index.js';
+		$file_to_module_name=[];
+		foreach(scandir(INC_DIR.'/node_modules-included/catpow/src/') as $fname){
+			if(substr($fname,0,1)!=='.' && file_exists(INC_DIR.'/node_modules-included/catpow/src/'.$fname.'/index.js')){
+				$files[]=$file=INC_DIR.'/node_modules-included/catpow/src/'.$fname.'/index.js';
+				$file_to_module_name[$file]=$fname;
 			}
 		}
-		exec('node modules/src/script/declaration.mjs -t module '.implode(' ',$files),$output);
-		return json_decode(implode("\n",$output));
+		exec('node node_modules-included/catpow/src/script/declaration.mjs -t module '.implode(' ',$files),$output);
+		$declaration=json_decode(implode("\n",$output),true);
+		$modules=[];
+		foreach($file_to_module_name as $file=>$module_name){
+			$modules[$module_name]=$declaration[$file];
+		}
+		return $modules;
 	}
 }
