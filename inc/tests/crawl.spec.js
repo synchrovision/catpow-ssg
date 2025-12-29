79923a8260df3b9de3d90a2cb2791a094522b13a
synchrovision <hatano@catpow.info>
2025-12-29 17:49:57 +0900

    feat 👍 update crawl.spec.js

--- inc/tests/crawl.spec.js
+++ inc/tests/crawl.spec.js
@@ -10,6 +10,12 @@ test("screenshot", async ({ page, browser }, testInfo) => {
 		testInfo.setTimeout(testInfo.timeout + 6000);
 		await page.goto(`http://localhost:8000${uri}`);
 		await page.waitForLoadState("networkidle");
+		if ((await page.locator("body[aria-busy]").count()) > 0) {
+			await page
+				.locator("body[aria-busy]")
+				.waitFor({ state: "detached", timeout: 5000 })
+				.catch(() => {});
+		}
 		if (uri.endsWith("/")) {
 			uri += "index.html";
 		}
